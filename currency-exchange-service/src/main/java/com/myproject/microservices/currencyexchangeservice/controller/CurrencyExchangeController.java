6066c3602e66c701a916e95d395d19b92b9a2ffe
unknown <kotovpetia@gmail.com>
2022-07-10 23:16:31 +0300

    zipkin added

--- currency-exchange-service/src/main/java/com/myproject/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/myproject/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.myproject.microservices.currencyexchangeservice.controller;
 
 import com.myproject.microservices.currencyexchangeservice.entity.CurrencyExchange;
 import com.myproject.microservices.currencyexchangeservice.service.CurrencyExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeService service;
 
@@ -23,6 +27,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = service.findByFromAndTo(from, to);
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
